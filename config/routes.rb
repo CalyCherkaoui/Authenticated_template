b639ccca7a06019a9d9e41d3b73224eaa0685e13
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-19 15:09:25 -0600

    Setup devise controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
+  get 'home/index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
 end
