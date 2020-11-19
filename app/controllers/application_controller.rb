112cfa071a7e1f826d8865910a32c67708fcb411
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-19 15:06:43 -0600

    setting up authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(_resource)
+    home_index_path
+  end
 end
