05833a2f141aaefbaa30c678c01a902ef4b7b464
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-11-19 16:03:34 -0600

    Adding navigation and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+
+  def flash_class_helper(type)
+    case type.to_sym
+    when :notice then "success"
+    when :success then "success"
+    when :error then "danger"
+    when :alert then "danger"
+    when :warning then "warning"
+    end
+  end
+
+  def flash_prefix_helper(type)
+    case type.to_sym
+    when :notice then "Notice!"
+    when :success then "Success!"
+    when :error then "Error!"
+    when :alert then "Alert!"
+    when :warning then "Warning!"
+    end 
+  end
 end
